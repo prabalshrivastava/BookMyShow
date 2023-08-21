0540d4e86c137273ee17b92859c1d3a8f30ebc9c
Prabal Shrivastava <prabalshrivastava54@gmail.com>
2023-08-21 15:03:32 +0530

    Added Entity Mapping.

--- src/main/java/com/bms/BookMyShow/dto/Ticket.java
+++ src/main/java/com/bms/BookMyShow/dto/Ticket.java
@@ -1,6 +1,8 @@
 package com.bms.BookMyShow.dto;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import java.util.List;
 import lombok.Data;
 
@@ -8,9 +10,24 @@ import lombok.Data;
 @Entity
 public class Ticket extends BaseModel {
 
+  /**
+   * 1 Ticket has multiple ShowSeat -> Ticket 999 has Avengers@5pm@Audi1@A1,Avengers@5pm@Audi1@A2,Avengers@5pm@Audi1@A3
+   * 1 ShowSeat is only associated with 1 ticket -> Avengers@5pm@Audi1@A1 is associated with the ticket 999
+   */
+  @OneToMany
   List<ShowSeat> showSeats;
   Float amount;
+  /**
+   * 1 Ticket has multiple Payment -> Ticket 999 has payment through Paytm@100,Phonepe@200,CC@300 ie total of 600rs through 3 portals.
+   * 1 Payment is only associated with 1 ticket -> Paytm@100 is associated with the ticket 999
+   */
+  @OneToMany
   List<Payment> payments;
   TicketStatus ticketStatus;
+  /**
+   * 1 Ticket has 1 User -> Ticket 999 belongs to Prabal
+   * 1 User can have multiple tickets -> Prabal has ticket 999,213
+   */
+  @ManyToOne
   User user;
 }
