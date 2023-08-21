23567a87e77aba0230642bd1043ad9fd2b7f6d65
Prabal Shrivastava <prabalshrivastava54@gmail.com>
2023-08-21 14:43:41 +0530

    Added Entity Mapping.

--- src/main/java/com/bms/BookMyShow/dto/Cast.java
+++ src/main/java/com/bms/BookMyShow/dto/Cast.java
@@ -1,6 +1,7 @@
 package com.bms.BookMyShow.dto;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
 import java.util.List;
 import lombok.Data;
 
@@ -9,5 +10,10 @@ import lombok.Data;
 public class Cast extends BaseModel {
   private Profile profile;
   private CastType castType;
+  /**
+   * 1 Cast can work in multiple Movies -> Robert Downey Jr worked on Iron Man (2008),Sherlock Holmes (2009),Chaplin (1992),Avengers: Endgame (2019)
+   * 1 Movie can have multiple casts -> Avengers has Robert Downey Jr,Chris Evans,Mark Ruffalo,Chris Hemsworth,Scarlett Johansson,Jeremy Renner,Tom Hiddleston,Clark Gregg
+   */
+  @ManyToMany
   private List<Movie> movies;
 }
