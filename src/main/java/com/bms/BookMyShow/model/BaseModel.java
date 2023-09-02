b66c7f09d405b2cccda31663a0287a229caf38a1
Prabal Shrivastava <prabalshrivastava54@gmail.com>
2023-09-02 20:12:41 +0530

    Added generate Ticket Logic

--- src/main/java/com/bms/BookMyShow/model/BaseModel.java
+++ src/main/java/com/bms/BookMyShow/model/BaseModel.java
@@ -1,12 +1,16 @@
 package com.bms.BookMyShow.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import lombok.Data;
+import lombok.Generated;
 
 @Data
 @MappedSuperclass
 public class BaseModel {
   @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private int id;
 }
