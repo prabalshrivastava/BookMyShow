269e77ced0c476f82f62fb8ed3d08f11b8facd10
Prabal Shrivastava <prabalshrivastava54@gmail.com>
2023-09-02 17:58:10 +0530

    Added Ticket Dto information

--- src/main/java/com/bms/BookMyShow/dto/TicketResponseDto.java
+++ src/main/java/com/bms/BookMyShow/dto/TicketResponseDto.java
@@ -1,5 +1,12 @@
 package com.bms.BookMyShow.dto;
 
-public class TicketResponseDto {
+import com.bms.BookMyShow.model.Ticket;
+import lombok.Data;
 
+@Data
+public class TicketResponseDto {
+  //What all information is needed in the ticket
+  //1. Show Details ie Movie and which City->Theater->Auditorium is the booking for, and it's timing this all information can be extracted from the show object
+  //2. Seats Details containing all the seats that are booked by the user.
+  public Ticket ticket;
 }
